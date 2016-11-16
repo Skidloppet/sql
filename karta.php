05b2947e9b6bf4ae7250771c6ff72a2155cd6113
n1owns <niklasolund@hotmail.com>
2016-11-16 15:21:43 +0100

    lade till kundDetaljer samt ändrade karta.php så den länkar till specifik delsträcka på  kundDetaljer

--- karta.php
+++ karta.php
@@ -35,6 +35,17 @@ Här kan man skriva kommentarer..
 
 Nedanför kan man skriva php kod..
 -->
+
+
+<div>
+	<a href="kundDetaljer.php">kundDetaljer.php</a>
+</div>
+
+
+
+
+
+<div>
 <?php
 	foreach($pdo->query( 'select * from karta order by rspName;' ) as $row){
 			    $SubPlaceNameArray[] = $row['rspName']; 
@@ -45,15 +56,20 @@ Nedanför kan man skriva php kod..
 	print_r($SubPlaceNameArray);
 	echo "</br>";
 	print_r($RatingArray);
+	echo "</br>";
+	echo "<h3>kanske är bättre att skapa en multidimentionell array?!</h3>";
+	echo 'exempel: $cars = array (array("Volvo",22,18), array("BMW",15,13), array("Saab",5,2), array("Land Rover",17,15));';
+	echo "</br></br></br>";
 
-	echo "</br>Här är delsträcka 1: "."$SubPlaceNameArray[0]".". med rating: "."$RatingArray[0]";
-	echo "</br>Här är delsträcka 2: "."$SubPlaceNameArray[1]".". med rating: "."$RatingArray[1]";
-	echo "</br>Här är delsträcka 3: "."$SubPlaceNameArray[2]".". med rating: "."$RatingArray[2]";
-	echo "</br>Här är delsträcka 4: "."$SubPlaceNameArray[3]".". med rating: "."$RatingArray[3]";
-	echo "</br>Här är delsträcka 5: "."$SubPlaceNameArray[4]".". med rating: "."$RatingArray[4]";
-	echo "</br>Här är delsträcka 6: "."$SubPlaceNameArray[5]".". med rating: "."$RatingArray[5]";
+
+	echo "<a href='kundDetaljer.php?DS=".$SubPlaceNameArray[0]."'>delsträcka # ".$SubPlaceNameArray[0]."</a> med rating: ".$RatingArray[0]."</br>";
+	echo "<a href='kundDetaljer.php?DS=".$SubPlaceNameArray[1]."'>delsträcka # ".$SubPlaceNameArray[1]."</a> med rating: ".$RatingArray[1]."</br>";
+	echo "<a href='kundDetaljer.php?DS=".$SubPlaceNameArray[2]."'>delsträcka # ".$SubPlaceNameArray[2]."</a> med rating: ".$RatingArray[2]."</br>";
+	echo "<a href='kundDetaljer.php?DS=".$SubPlaceNameArray[3]."'>delsträcka # ".$SubPlaceNameArray[3]."</a> med rating: ".$RatingArray[3]."</br>";
+	echo "<a href='kundDetaljer.php?DS=".$SubPlaceNameArray[4]."'>delsträcka # ".$SubPlaceNameArray[4]."</a> med rating: ".$RatingArray[4]."</br>";
+	echo "<a href='kundDetaljer.php?DS=".$SubPlaceNameArray[5]."'>delsträcka # ".$SubPlaceNameArray[5]."</a> med rating: ".$RatingArray[5]."</br>";
 
 ?>
-
+</div>
 </body>
 </html>
