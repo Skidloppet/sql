6a4e5ec85974137a63798da4d76ea78dd53e1cb1
Niklas <niklasolund@hotmail.com>
2016-11-20 19:05:24 +0100

    tog bort connectfilen..

--- backend_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-			$pdo = new PDO('mysql:dbname=SlitABSkidloppet;host=localhost','sqllab','Tomten2009');
-		    $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
-
-		    $conn = mysqli_connect("localhost","sqllab","Tomten2009",'SlitABSkidloppet');
-			if (!$conn) {
-			die("Connection failed:".mysqli_connect_error());
-}
