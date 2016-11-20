9de833a179aff8796f7958aa10eb91d38b714e4f
frostborn88 <sebastian.persic@gmail.com>
2016-11-20 15:17:01 +0100

    Add files via upload

---
+++ backend_connect.php
@@ -0,0 +1,8 @@
+<?php
+			$pdo = new PDO('mysql:dbname=SlitABSkidloppet;host=localhost','sqllab','Tomten2009');
+		    $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+
+		    $conn = mysqli_connect("localhost","sqllab","Tomten2009",'SlitABSkidloppet');
+			if (!$conn) {
+			die("Connection failed:".mysqli_connect_error());
+}
