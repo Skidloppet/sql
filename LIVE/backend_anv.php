24cc984786b8e8ad21b89ba3f15df607fa2ad716
Niklas <niklasolund@hotmail.com>
2016-11-22 23:29:36 +0100

    implementerade nya funktioner i backend_anv.php

--- LIVE/backend_anv.php
+++ LIVE/backend_anv.php
@@ -1,3 +1,6 @@
+<?php
+SESSION_START();
+?>
 <!DOCTYPE html>
 <html>
 <title>Skidloppet AB - Monitor</title>
@@ -11,186 +14,151 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
 </style>
 <body class="w3-light-grey">
 
-<!-- Top container -->
-<div class="w3-container w3-top w3-black w3-large w3-padding" style="z-index:4">
-  <button class="w3-btn w3-hide-large w3-padding-0 w3-hover-text-grey" onclick="w3_open();"><i class="fa fa-bars"></i>  Meny</button>
-  <span class="w3-right">Skidloppet AB</span>
-</div>
+  <!-- Top container -->
+  <div class="w3-container w3-top w3-black w3-large w3-padding" style="z-index:4">
+    <button class="w3-btn w3-hide-large w3-padding-0 w3-hover-text-grey" onclick="w3_open();"><i class="fa fa-bars"></i>  Meny</button>
+    <span class="w3-right">Skidloppet AB</span>
+  </div>
 
-<!-- Sidenav/menu -->
-<?php
-	include 'backend_sidemenu.php';
-?>
-
-
-<!-- Overlay effect when opening sidenav on small screens -->
-<div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
-
-<!-- !PAGE CONTENT! -->
-<div class="w3-main" style="margin-left:300px;margin-top:43px;">
-
-  <!-- Header -->
+  <!-- Sidenav/menu -->
   <?php
-	include 'backend_headerbox.php';
-	?>
+  include 'backend_sidemenu.php';
+  ?>
+  <!-- Overlay effect when opening sidenav on small screens -->
+  <div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+  <!-- !PAGE CONTENT! -->
+  <div class="w3-main" style="margin-left:300px;margin-top:43px;">
+    <!-- Header -->
+    <?php
+    include 'backend_headerbox.php';
+    ?>
 
-  <div class="w3-container w3-section">
-    <div class="w3-row-padding" style="margin:0 -16px">
-      <div class="w3-threethird">
-        <h5>Karta</h5>
-	<?php
-		include 'backend_map.php';
-		?>
-        <!-- <img src="images/karta_kund.svg" style="width:900px" "height:20%" alt="karta"> -->
-      </div>
-	  
-	  <div class="w3-threethird">
-      <?php
-		include 'backend_dsstatus.php';
-		?>
-  </div>
-      
-	  <div class="w3-threethird">
-      <?php
-		include 'backend_feeds.php';
-		?>
-  </div>
-  <hr>
-  <div class="w3-container">
-    <h5>Generella betyg hela arenan</h5>
-    <p>Underlag</p>
-    <div class="w3-progress-container w3-grey">
-      <div id="myBar" class="w3-progressbar w3-red" style="width:25%">
-        <div class="w3-center w3-text-white">2</div>
-      </div>
-    </div>
 
-    <p>Spårkanter</p>
-    <div class="w3-progress-container w3-grey">
-      <div id="myBar" class="w3-progressbar w3-orange" style="width:50%">
-        <div class="w3-center w3-text-white">3</div>
-      </div>
-    </div>
 
-    <p>Stavfäste</p>
-    <div class="w3-progress-container w3-grey">
-      <div id="myBar" class="w3-progressbar w3-green" style="width:75%">
-        <div class="w3-center w3-text-white">4</div>
-      </div>
-    </div>
-  </div>
-  <hr>
+    <!-- ny ski roll -->
+    <?php
+    include'connect.php';
+    ?>
+    <div>
+      <h3>Sätt ny roll för en Skidloppet användare</h3>
+      <h4>fel när det inte finns någon av en viss typ(då försvinner alternativet)</h4>
+      <form action='<?php $_PHP_SELF ?>' method='POST'>
+        <select size='1' name='skiID'>
+          <option selected="selected"> Choose employee </option>
+          <?php    
+          foreach($pdo->query( 'SELECT * FROM Ski ORDER BY skiID;' ) as $row){
+            echo '<option value="'.$row['skiID'].'">';
+            echo $row['firstName']." ".$row['lastName']." (".$row['type'].")";      
+            echo '</option>';
+          }    
+          ?>
+        </select>
+        <select size='1' name='type'>
+          <option selected="selected"> New role </option>
+          <?php    
+          foreach($pdo->query( 'SELECT type FROM Ski GROUP BY type;' ) as $row){
+          # Denna bör ändras då den inte är fullt funktionell.
+            echo '<option value="'.$row['type'].'">';
+            echo $row['type'];      
+            echo '</option>';
+          }    
+          ?>
+        </select>
+        <input type="submit" value="Send" name="send">
+        <input type="reset">
 
-  <div class="w3-container">
-    <h5>Countries</h5>
-    <table class="w3-table w3-striped w3-bordered w3-border w3-hoverable w3-white">
-      <tr>
-        <td>United States</td>
-        <td>65%</td>
-      </tr>
-      <tr>
-        <td>UK</td>
-        <td>15.7%</td>
-      </tr>
-      <tr>
-        <td>Russia</td>
-        <td>5.6%</td>
-      </tr>
-      <tr>
-        <td>Spain</td>
-        <td>2.1%</td>
-      </tr>
-      <tr>
-        <td>India</td>
-        <td>1.9%</td>
-      </tr>
-      <tr>
-        <td>France</td>
-        <td>1.5%</td>
-      </tr>
-    </table><br>
-    <button class="w3-btn">More Countries  <i class="fa fa-arrow-right"></i></button>
-  </div>
-  <hr>
-  <div class="w3-container">
-    <h5>Recent Users</h5>
-    <ul class="w3-ul w3-card-4 w3-white">
-      <li class="w3-padding-16">
-        <span onclick="this.parentElement.style.display='none'" class="w3-closebtn w3-padding w3-margin-right w3-medium">x</span>
-        <img src="/w3images/avatar2.png" class="w3-left w3-circle w3-margin-right" style="width:35px">
-        <span class="w3-xlarge">Mike</span><br>
-      </li>
-      <li class="w3-padding-16">
-        <span onclick="this.parentElement.style.display='none'" class="w3-closebtn w3-padding w3-margin-right w3-medium">x</span>
-        <img src="/w3images/avatar5.png" class="w3-left w3-circle w3-margin-right" style="width:35px">
-        <span class="w3-xlarge">Jill</span><br>
-      </li>
-      <li class="w3-padding-16">
-        <span onclick="this.parentElement.style.display='none'" class="w3-closebtn w3-padding w3-margin-right w3-medium">x</span>
-        <img src="/w3images/avatar6.png" class="w3-left w3-circle w3-margin-right" style="width:35px">
-        <span class="w3-xlarge">Jane</span><br>
-      </li>
-    </ul>
-  </div>
-  <hr>
+        <?php 
+        if(isset($_POST['send'])){
+          $querystring='Call EditSki (:editSkiID, :newType);';
+          $stmt = $pdo->prepare($querystring);
+          $stmt->bindParam(':editSkiID', $_POST['skiID']);
+          # select's name är skiID
+          $stmt->bindParam(':newType', $_POST['type']);
+          # select's name är type
+          $stmt->execute();
 
-  <div class="w3-container">
-    <h5>Senaste kommentarerna</h5>
-    <div class="w3-row">
-      <div class="w3-col m2 text-center">
-        <img class="w3-circle" src="/w3images/avatar3.png" style="width:96px;height:96px">
+  #uppdaterar sidan och visar nya lönen
+        //  header("Location: hemsida5.php");
+        }
+        ?>
       </div>
-      <div class="w3-col m10 w3-container">
-        <h4>Kund <span class="w3-opacity w3-medium">Sep 29, 2014, 9:12 PM</span></h4>
-        <p>Keep up the GREAT work! I am cheering for you!! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><br>
-      </div>
-    </div>
 
-    <div class="w3-row">
-      <div class="w3-col m2 text-center">
-        <img class="w3-circle" src="/w3images/avatar1.png" style="width:96px;height:96px">
-      </div>
-      <div class="w3-col m10 w3-container">
-        <h4>Jonas <span class="w3-opacity w3-medium">Sep 28, 2014, 10:15 PM</span></h4>
-        <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><br>
-      </div>
-    </div>
-  </div>
-  <br>
-  <div class="w3-container w3-dark-grey w3-padding-32">
-    <div class="w3-row">
-      <div class="w3-container w3-third">
-        <h5 class="w3-bottombar w3-border-green">Demographic</h5>
-        <p>Language</p>
-        <p>Country</p>
-        <p>City</p>
-      </div>
-      <div class="w3-container w3-third">
-        <h5 class="w3-bottombar w3-border-red">System</h5>
-        <p>Browser</p>
-        <p>OS</p>
-        <p>More</p>
-      </div>
-      <div class="w3-container w3-third">
-        <h5 class="w3-bottombar w3-border-orange">Target</h5>
-        <p>Users</p>
-        <p>Active</p>
-        <p>Geo</p>
-        <p>Interests</p>
-      </div>
-    </div>
-  </div>
 
-  <!-- Footer -->
-  <footer class="w3-container w3-padding-16 w3-light-grey">
-    <h4>FOOTER</h4>
-    <p>Powered by <a href="http://www.his.se" target="_blank">SLITAB</a></p>
-  </footer>
+      <!-- ny ent användare -->
+      <div>
+        <h3>Create new Ent</h3>
+        <form action='<?php $_PHP_SELF ?>' method='POST'>
+          <input type="text" name="firstName" placeholder="first name.."></p>
+          <input type="text" name="lastName" placeholder="surname.."></p>
+          <input type="text" name="email" placeholder="email.."></p>
+          <input type="text" name="number" placeholder="number.."></p>
+          <input type="password" name="pass" placeholder="password.."></p>
+          <p><button type="submit" name="CreateEnt" id="CreateEnt">NEW ENT</button></p></form>
 
-  <!-- End page content -->
-</div>
 
-<script>
+          <?php
+             # skapa ett errormedelande vid fel input (email, number)
+
+          if(isset($_POST['CreateEnt'])){
+
+            $sql = "CALL CreateEnt(:pass, :firstName, :lastName ,:email, :number)";
+
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(":pass", $_POST['pass'], PDO::PARAM_STR);
+            $stmt->bindParam(":firstName", $_POST['firstName'], PDO::PARAM_STR);
+            $stmt->bindParam(":lastName", $_POST['lastName'], PDO::PARAM_STR);
+            $stmt->bindParam(":email", $_POST['email'], PDO::PARAM_STR);
+            $stmt->bindParam(":number", $_POST['number'], PDO::PARAM_INT);
+            $stmt->execute();
+          }     
+          ?>
+        </div>
+
+
+        <!-- ny ski användare -->
+        <div>
+          <h3>Create new Ski</h3>
+          <form action='<?php $_PHP_SELF ?>' method='POST'>
+            <input type="text" name="firstName" placeholder="first name..">
+            <input type="text" name="lastName" placeholder="surname..">
+            <input type="text" name="email" placeholder="email..">
+            <input type="text" name="number" placeholder="number..">
+            <input type="text" name="type" placeholder="type..">
+            <input type="text" name="pass" placeholder="password..">
+            <button type="submit" name="CreateSki" id="CreateSki">NEW SKI</button>
+          </form>
+
+
+          <?php
+
+          if(isset($_POST['CreateSki'])){
+
+            $sql = "CALL CreateSki( :pass, :firstName, :lastName , :email, :number, :type)";
+
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(":pass", $_POST['pass'], PDO::PARAM_STR);
+            $stmt->bindParam(":firstName", $_POST['firstName'], PDO::PARAM_STR);
+            $stmt->bindParam(":lastName", $_POST['lastName'], PDO::PARAM_STR);
+            $stmt->bindParam(":email", $_POST['email'], PDO::PARAM_STR);
+            $stmt->bindParam(":number", $_POST['number'], PDO::PARAM_INT);
+            $stmt->bindParam(":type", $_POST['type'], PDO::PARAM_STR);
+            $stmt->execute();
+          }     
+          ?>
+        </div>
+
+
+
+        <!-- Footer -->
+        <footer class="w3-container w3-padding-16 w3-light-grey">
+          <h4>FOOTER</h4>
+          <p>Powered by <a href="http://www.his.se" target="_blank">SLITAB</a></p>
+        </footer>
+
+        <!-- End page content -->
+      </div>
+
+      <script>
 // Get the Sidenav
 var mySidenav = document.getElementById("mySidenav");
 
@@ -199,19 +167,19 @@ var overlayBg = document.getElementById("myOverlay");
 
 // Toggle between showing and hiding the sidenav, and add overlay effect
 function w3_open() {
-    if (mySidenav.style.display === 'block') {
-        mySidenav.style.display = 'none';
-        overlayBg.style.display = "none";
-    } else {
-        mySidenav.style.display = 'block';
-        overlayBg.style.display = "block";
-    }
+  if (mySidenav.style.display === 'block') {
+    mySidenav.style.display = 'none';
+    overlayBg.style.display = "none";
+  } else {
+    mySidenav.style.display = 'block';
+    overlayBg.style.display = "block";
+  }
 }
 
 // Close the sidenav with the close button
 function w3_close() {
-    mySidenav.style.display = "none";
-    overlayBg.style.display = "none";
+  mySidenav.style.display = "none";
+  overlayBg.style.display = "none";
 }
 </script>
 
