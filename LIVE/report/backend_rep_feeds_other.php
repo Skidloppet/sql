82c5e5090f82d8a070a201a971d10ffe98326e29
JakobSpoe <jakob.soderberg@hotmail.com>
2016-11-24 12:55:52 +0100

    Restrictions till rapport

---
+++ LIVE/report/backend_rep_feeds_other.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+
+<?php
+include'connect.php';
+#include './backend_connect.php';
+?>
+<div class="w3-container">
+    <h3>Utskrift av rapporter</h3>
+    <table border="1">
+      <?php   
+        echo "<tr>";
+          echo "<th>reportID</th>"; 
+          echo "<th>newEntID</th>"; 
+          echo "<th>newStartDate</th>"; 
+          echo "<th>newWorkDate</th>"; 
+          echo "<th>newRating</th>"; 
+          echo "<th>newUnderlay</th>"; 
+          echo "<th>newEdges</th>"; 
+          echo "<th>newGrip</th>"; 
+          echo "<th>newDepth</th>";
+          echo "<th>kommentar</th>";
+          echo "<th>sträcka</th>";  
+          echo "</tr>";
+
+        foreach($pdo->query( 'SELECT * FROM Reporting;' ) as $row){
+          //echo "<tr><td>";
+          //echo "<a href='test.php?entID=".urlencode($row['entID'])."'>".$row['entID'];
+          echo "<tr>";
+          echo "<td>".$row['reportID']."</td>";
+          echo "<td>".$row['entID']."</td>";
+          echo "<td>".$row['startDate']."</td>";
+          echo "<td>".$row['workDate']."</td>";
+          echo "<td>".$row['rating']."</td>";
+          echo "<td>".$row['underlay']."</td>";
+          echo "<td>".$row['edges']."</td>";
+          echo "<td>".$row['grip']."</td>";
+          echo "<td>".$row['depth']."</td>";
+          echo "<td>".$row['comment']."</td>";
+          echo "<td>".$row['name']."</td>";
+          ?>
+
+<div class="w3-container">
+    <h3>Utskrift av sparade rapporter</h3>
+    <table border="1">
+      <?php   
+        echo "<tr>";
+          echo "<th>reportID</th>"; 
+          echo "<th>newEntID</th>"; 
+          echo "<th>newStartDate</th>"; 
+          echo "<th>newWorkDate</th>"; 
+          echo "<th>newRating</th>"; 
+          echo "<th>newUnderlay</th>"; 
+          echo "<th>newEdges</th>"; 
+          echo "<th>newGrip</th>"; 
+          echo "<th>newDepth</th>";
+          echo "<th>kommentar</th>";
+          echo "<th>sträcka</th>";  
+          echo "</tr>";
+        foreach($pdo->query( 'SELECT * FROM StoredReports;' ) as $row){
+          //echo "<tr><td>";
+          //echo "<a href='test.php?entID=".urlencode($row['entID'])."'>".$row['entID'];
+          echo "<tr>";
+          echo "<td>".$row['reportID']."</td>";
+          echo "<td>".$row['entID']."</td>";
+          echo "<td>".$row['startDate']."</td>";
+          echo "<td>".$row['workDate']."</td>";
+          echo "<td>".$row['rating']."</td>";
+          echo "<td>".$row['underlay']."</td>";
+          echo "<td>".$row['edges']."</td>";
+          echo "<td>".$row['grip']."</td>";
+          echo "<td>".$row['depth']."</td>";
+          echo "<td>".$row['comment']."</td>";
+          echo "<td>".$row['name']."</td>";
+}
+?>
+</table>
+<br><br>
+ <!-- 
+<div class="w3-container w3-dark-grey w3-padding-32">
+  <div class="w3-row">
+    <div class="w3-container w3-third">
+      <h5 class="w3-bottombar w3-border-green">Demographic</h5>
+      <p>Language</p>
+      <p>Country</p>
+      <p>City</p>
+    </div>
+    <div class="w3-container w3-third">
+      <h5 class="w3-bottombar w3-border-red">System</h5>
+      <p>Browser</p>
+      <p>OS</p>
+      <p>More</p>
+    </div>
+    <div class="w3-container w3-third">
+      <h5 class="w3-bottombar w3-border-orange">Target</h5>
+      <p>Users</p>
+      <p>Active</p>
+      <p>Geo</p>
+      <p>Interests</p>
+    </div>
+  </div>
+</div>
+-->
+<!-- Footer -->
+<footer class="w3-container w3-padding-16 w3-light-grey">
+  <!-- <h4>FOOTER</h4> -->
+  <p>Powered by <a href="http://www.his.se" target="_blank">SLITAB</a></p>
+</footer>
+
+<!-- End page content -->
+</div>
+</div>
+</div>
+
+<script>
+// Get the Sidenav
+var mySidenav = document.getElementById("mySidenav");
+
+// Get the DIV with overlay effect
+var overlayBg = document.getElementById("myOverlay");
+
+// Toggle between showing and hiding the sidenav, and add overlay effect
+function w3_open() {
+  if (mySidenav.style.display === 'block') {
+    mySidenav.style.display = 'none';
+    overlayBg.style.display = "none";
+  } else {
+    mySidenav.style.display = 'block';
+    overlayBg.style.display = "block";
+  }
+}
+
+// Close the sidenav with the close button
+function w3_close() {
+  mySidenav.style.display = "none";
+  overlayBg.style.display = "none";
+}
+</script>
+
+</body>
+</html>
