29e527095223f56d581cef1afb3e7a0323bdd3a3
n1owns <niklasolund@hotmail.com>
2016-11-30 19:33:23 +0100

    index feed börjar bli klar.

--- LIVE/includes/index/ind_feeds_arenachef.php
+++ LIVE/includes/index/ind_feeds_arenachef.php
@@ -25,15 +25,13 @@ if(isset($_GET['orderID2'])){
 }
 ?>
 
-
-
 <?php
 $akut = 0;
 foreach($pdo->query( 'SELECT count(*) as nmr FROM wo where priority="akut" and entID="1";' ) as $row){
   $akut = $row['nmr'];
   if (0 < $akut){
     ?>
-    <div class="w3-container w3-orange" style="border-color:lightblue; border-style: solid; border-width: 5px 5px 0px 5px;">
+    <div class="w3-container w3-orange" style="border-color:lightblue; border-style: solid; border-width: 5px;">
       <div class="w3-container w3-section">
         <div class="w3-row-padding" style="margin:0 -16px">
           <div class="w3-threethird">
@@ -49,8 +47,6 @@ foreach($pdo->query( 'SELECT count(*) as nmr FROM wo where priority="akut" and e
                 <th>Skapad av</th>
                 <th>Ange entrepenör</th>
               </tr> 
-
-
 
               <?php     
         # hämtar alla aktiva arbetsordrar(WorkOrder) som tillhör det autoangivna akutID (#1)
@@ -101,26 +97,16 @@ foreach($pdo->query( 'SELECT count(*) as nmr FROM wo where priority="akut" and e
 }
 ?>
 
+<h2>Översikt</h2>
 
 <div  class="w3-threethird" style="border-color:lightblue; border-style: solid; border-width: 5px;">
   <?php
   include './map.php';
   ?>
 </div>
-
-
-
-
-
-
 </div>
 </div>
 
-
-
-<!-- 
-frågan om att byta entrepenör ansvarig för arbetsorder till DB, fungerar ej.(i tabellen)
--->
 <?php
 if(isset($_POST['newEnt'])){
   $sql = "call _newResponsability (:_entID,:_orderID)";
@@ -132,8 +118,8 @@ if(isset($_POST['newEnt'])){
 ?>
 </div>
 </div>
-<h2>Översikt</h2>
-<div class="w3-row-padding" style="margin:0 -16px">
+
+<div class="w3-row-padding" style="border-color:lightblue; border-style: solid; border-width: 5px;">
   <div class="w3-threethird">
     <h5>Pågående arbetsordrar</h5>
     <table class="w3-table w3-striped w3-white">
@@ -167,5 +153,48 @@ if(isset($_POST['newEnt'])){
       ?>   
     </table>
   </div>
+
 </div>
+
+
+<div class="w3-row-padding" style="border-color:lightblue; border-style: solid; border-width: 5px; margin-top:15px;">
+  <div class="w3-threethird">
+    <h5>Senaste avklarade rapporterna (max 5)</h5>
+    <table class="w3-table w3-striped w3-white">
+      <tr>
+        <th><i class="fa fa-users w3-orange w3-text-white w3-padding-tiny"></i></th>
+        <th>Report-ID</th>
+        <th>Entrepenör</th>
+        <th>Avklarad</th>
+        <th>Nästa planerade pass</th>
+        <th>Betyg</th>
+        <th>Underlag</th>
+        <th>Spårkanter</th>
+        <th>Stavfäste</th>
+        <th>Snödjup</th>
+        <th>Kommentar</th>
+        <th>Spårade delsträckor</th>
+      </tr>        
+      <?php     
+
+      foreach($pdo->query( 'select * from storedR;' ) as $row){
+        echo "<tr><td><i class='fa fa-eye w3-blue w3-padding-tiny'></i></td>";
+        echo "<td>".$row['reportID']."</td>";
+        echo "<td>".$row['firstName']." ".$row['lastName']." ( ".$row['entID']." )</td>";
+        echo "<td>".$row['startDate']."</td>";
+        echo "<td>".$row['workDate']."</td>";
+        echo "<td>".$row['rating']."</td>";
+        echo "<td>".$row['underlay']."</td>";
+        echo "<td>".$row['edges']."</td>";
+        echo "<td>".$row['grip']."</td>";
+        echo "<td>".$row['depth']."</td>";
+        echo "<td>".$row['comment']."</td>";
+        echo "<td>tom..foreach alla N:M</td>";
+        echo "</tr>";  
+      }
+      ?>   
+       </table>
+  </div>
+
 </div>
+
