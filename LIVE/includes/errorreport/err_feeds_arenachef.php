994e491d1d05c8f979178d1c2fcdf88e9ab7a7b5
n1owns <niklasolund@hotmail.com>
2016-12-02 16:24:20 +0100

    uppd. felanmälan

--- LIVE/includes/errorreport/err_feeds_arenachef.php
+++ LIVE/includes/errorreport/err_feeds_arenachef.php
@@ -5,125 +5,102 @@ include'../connect.php';
 
 
 <div id="12" class="w3-container w3-blue">
-    <h3>Utskrift av registrerade felanmälningar</h3>
-    <table class="w3-table w3-striped w3-white">
-      <?php  
-            echo "<tr>";
+  <h3>Utskrift av registrerade felanmälningar</h3>
+  <table class="w3-table w3-striped w3-white">
+    <?php  
+    echo "<tr>";
             #echo "<th style='background-color:white;'>Sträcka:</th>";
-            echo "<th style='background-color:white;'>Sträcka:</th>"; 
+    echo "<th style='background-color:white;'>Sträcka:</th>"; 
             #echo "<th style='background-color:white;'>entID:</th>";
-            echo "<th style='background-color:white;'>Entreprenör:</th>";
-            echo "<th style='background-color:white;'>Skickad:</th>";
-            echo "<th style='background-color:white;'>Beskrivning:</th>";
-            echo "<th style='background-color:white;'>Typ:</th>";
-            echo "<th style='background-color:white;'>Error ID:</th>";
-            echo "</tr>";
+    echo "<th style='background-color:white;'>Entreprenör:</th>";
+    echo "<th style='background-color:white;'>Skickad:</th>";
+    echo "<th style='background-color:white;'>Beskrivning:</th>";
+    echo "<th style='background-color:white;'>Typ:</th>";
+    echo "<th style='background-color:white;'>Error ID:</th>";
+    echo "</tr>";
 
-          foreach($pdo->query( 'SELECT * 
-            FROM Error, ErrorSubPlace, Ent, SubPlace
-            WHERE Error.errorID = ErrorSubPlace.errorID AND Ent.entID = Error.entID and SubPlace.name = ErrorSubPlace.name;
-            ' ) as $row){
-            echo "<tr>";
+    foreach($pdo->query( 'SELECT * 
+      FROM Error, ErrorSubPlace, Ent, SubPlace
+      WHERE Error.errorID = ErrorSubPlace.errorID AND Ent.entID = Error.entID and SubPlace.name = ErrorSubPlace.name;
+      ' ) as $row){
+      echo "<tr>";
             #echo "<td>".$row['name']."</td>";
-            echo "<td>".$row['realName']."</td>";
+    echo "<td>".$row['realName']."</td>";
             #echo "<td>".$row['entID']."</td>";
-            echo "<td>".$row['firstName']." ".$row['lastName']."</td>";
-            echo "<td>".$row['sentDate']."</td>";
-            echo "<td>".$row['errorDesc']."</td>";
-            echo "<td>".$row['type']."</td>";
-            echo "<td>".$row['errorID']."</td>";
-            ?>
-            <td class="Error-delete">
-        <form action='<?php echo $_SERVER['SCRIPT_NAME']; ?>' method='POST'>
-              <input type="hidden" name="deleteError" value="<?php echo $row['errorID']; ?>">
-              <input class="HoverButton" type="submit" name="delComment" value="Delete">
-            </form>
-          </td>
-          <?php
-            echo "</tr>";  
-        }
-      ?>
-    </table>
-    <br><br>
+    echo "<td>".$row['firstName']." ".$row['lastName']."</td>";
+    echo "<td>".$row['sentDate']."</td>";
+    echo "<td>".$row['errorDesc']."</td>";
+    echo "<td>".$row['type']."</td>";
+    echo "<td>".$row['errorID']."</td>";
+    ?>
+    <td class="Error-delete">
+      <form action='<?php echo $_SERVER['SCRIPT_NAME']; ?>' method='POST'>
+        <input type="hidden" name="deleteError" value="<?php echo $row['errorID']; ?>">
+        <input class="HoverButton" type="submit" name="delComment" value="Delete">
+      </form>
+    </td>
+    <?php
+    echo "</tr>";  
+  }
+  ?>
+</table>
+<br><br>
 </div>
 
 <?php
-  if(isset($_POST['delError'])){
+if(isset($_POST['delError'])){
   $deletedError = $_POST['deleteError'];
   $sql = "DELETE FROM Error WHERE ErrorID = $deletedError" ;
   $sql = "DELETE FROM ErrorSubPlace WHERE errorId = $deletedError" ;
   $stmt = $pdo->prepare($sql);
   $stmt->execute();
-    }
+}
 ?>
 
 <div id="11" class="w3-container w3-red">
   <h3>Ny felanmälan!</h3>
-        <form action='<?php echo $_SERVER['SCRIPT_NAME']; ?>' method='POST'>
+  <form action='<?php echo $_SERVER['SCRIPT_NAME']; ?>' method='POST'>
     <textarea rows="5" cols="70" name="desc" placeholder="Beskriv problemet..."></textarea>
-    </br>
-    <!--<p>Ange graden av problemets påverkan</p>-->
+  </br>
 
-    <!--<select name='grade'>
-      <option selected="selected"> Sätt nivå</option>
-      <option value="low">Lågt - Påverkar knappt</option>
-      <option value="medium">Medium - Påverkar en del</option>
-      <option value="high">Hög - Påverkar mycket</option>
-      <option value="akut">Akut - Grovt problem</option>
-    </select>
-    <br><br> -->
-    <p>Ange problemets typ:</p>
-    <select name='type'>
-      <option selected="selected"> Sätt typ</option>
-      <option value="lights">Ljus - Lyktstolpar, liknande</option>
-      <option value="tracks">Bana - Problem med banan</option>
-      <option value="dirt">Smuts - Skräp, kottar</option>
-      <option value="trees">Träd - Som påverkar banan</option>
-      <option value="other">Annat</option>
-    </select>
-    <br>
-
-
-    <!--<input type="text" name="type" placeholder="type.."></p>-->
-
-
-    <!-- Ändra till session! (Entreprenör) -->
-    <!--<p>Ange ansvarig entreprenörs id, ex: 1:</p>
-    <input type="text" name="entID" placeholder="entID.."></p>
-    -->
-
-    <!-- Listbox till att välja startsträcka-->
-    <p>Vart startade problemet?:</p>
-    <select name='Start'>    
-      <?php 
-      foreach ($pdo->query('SELECT * FROM SubPlace') as $row) {
-        echo '<option value="'.$row['name'].'">';
-        echo $row['realName'];
-        echo "</option>";
-      }
+  <p>Ange problemets typ *</p>
+  <select name="type">
+    <?php
+    $sql = 'SHOW COLUMNS FROM Error WHERE field="type"';
+    $row = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    foreach(explode("','",substr($row['Type'],6,-2)) as $option) {
+      print("<option>$option</option>");
+    }
     ?>
-    </select><br>
+  </select>
 
-
-    <!--<input type="text" name="Start" placeholder="Start.."></p>-->
-
-
-    <!-- Listbox till att välja slutsträcka-->
-    <p>Vart slutar problemets inverkan?:</p>
-        <select name='Slut'>    
-      <?php 
-      foreach ($pdo->query('SELECT * FROM SubPlace') as $row) {
-        echo '<option value="'.$row['name'].'">';
-        echo $row['realName'];
-        echo "</option>";
-      }
+  <!-- Listbox till att välja startsträcka-->
+  <p>Vart startade problemet?:</p>
+  <select name='Start'>    
+    <?php 
+    foreach ($pdo->query('SELECT * FROM SubPlace') as $row) {
+      echo '<option value="'.$row['name'].'">';
+      echo $row['realName'];
+      echo "</option>";
+    }
     ?>
-    </select><br><br>
+  </select><br>
+  <!-- Listbox till att välja slutsträcka-->
+  <p>Vart slutar problemets inverkan?:</p>
+  <select name='Slut'>    
+    <?php 
+    foreach ($pdo->query('SELECT * FROM SubPlace') as $row) {
+      echo '<option value="'.$row['name'].'">';
+      echo $row['realName'];
+      echo "</option>";
+    }
+    ?>
+  </select><br><br>
 
-    <!--<input type="text" name="Slut" placeholder="Slut.."></p>-->
+  <!--<input type="text" name="Slut" placeholder="Slut.."></p>-->
 
 
-    <p><button type="submit" name="Error">Ny felanmälan</button></p></form>
+  <p><button type="submit" name="Error">Ny felanmälan</button></p></form>
 
 
   <?php
@@ -141,8 +118,8 @@ include'../connect.php';
     $stmt->bindParam(":startName", $_POST['Start'], PDO::PARAM_INT);
     $stmt->bindParam(":endName", $_POST['Slut'], PDO::PARAM_INT);
     $stmt->execute();
-}    
-?>
+  }    
+  ?>
 </div>
 <!-- End page content -->
 </div>
