aaa2ad1908ac6fd2e014835b2318100299215c14
Patrik sjögren <psjgren7@gmail.com>
2016-12-05 16:28:13 +0100

    ss

--- LIVE/includes/index/ind_feeds_ent.php
+++ LIVE/includes/index/ind_feeds_ent.php
@@ -51,7 +51,7 @@ $stmt->execute();
               <div class="w3-row-padding" style="margin:0 -16px">
                 <div class="w3-threethird">
                   <h1>Akuta arbetsordrar</h1>
-                  <table class="w3-table w3-striped w3-white">
+                  <table class="w3-table w3-striped w3-bordered w3-border w3-hoverable w3-white">
                     <tr>
                       <th><u>Datum skickad</u></th>
                       <th>Arbetsorder-Typ</th>
@@ -123,7 +123,7 @@ $stmt->execute();
 <div class="w3-row-padding w3-panel w3-card-8 w3-round-xlarge" style=" border-color:lightblue; border-style: solid; border-width: 5px;">
       <div class="w3-threethird">
         <h5>Pågående arbetsordrar</h5>
-        <table class="w3-table w3-striped w3-white">
+        <table class="w3-table w3-striped w3-bordered w3-border w3-hoverable w3-white">
           <tr>
             <th><u>Datum skickad</u></th>
             <th>Område(n)</th>
