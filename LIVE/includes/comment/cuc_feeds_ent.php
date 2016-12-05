cc262a180ad3e43fee49ddab17ecb37f17ee622f
Patrik sjögren <psjgren7@gmail.com>
2016-12-05 19:28:24 +0100

    cuc ent

--- LIVE/includes/comment/cuc_feeds_ent.php
+++ LIVE/includes/comment/cuc_feeds_ent.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <?php
 include'../connect.php';
+
+
+foreach($pdo->query( 'select count(*)as d from co;') as $row){
+  $d = $row['d'];
+}
+
 ?>
 
- 
 
 <!--
 if(isset($_GET['commentID'])){
@@ -15,61 +20,60 @@ if(isset($_GET['commentID'])){
 
 }
 -->
-    <?php
-  if(isset($_POST['commentID'])){
-  $sql = "DELETE FROM Commenta WHERE commentID = :commentID";
+<?php
+if(isset($_POST['commentID'])){
+  $sql = "call _deleteCOM (:commentID)";
   $stmt = $pdo->prepare($sql);
   $stmt->bindParam(':commentID', $_POST['commentID'], PDO::PARAM_INT);
   $stmt->execute();
 
-    }
-  ?>
-  
-<div class="w3-row-padding" style="border-color:lightblue; border-style: solid; border-width: 5px;">
-  <div class="w3-threethird">
-      <h3>Kund Kommentarer</h3>
-      <table class="w3-table w3-striped w3-bordered w3-border w3-hoverable w3-white">
-        <tr>
-          <tr>
-          <th><i class="fa fa-users w3-orange w3-text-white w3-padding-tiny"></i></th>
-          <th>Alias</th>
-		  <th>Påverkade sträckor</th>
-          <th>Betyg</th>
-		  <th>Kommentar</th>
-		  <th>Datum</th>
-		  <th></th>
-                
-        <?php     
+}
+?>
 
-        foreach($pdo->query( 'SELECT * FROM Commenta order by commentID desc;' ) as $row){
-		  $luck = $row ['commentID'];
-          echo "<tr><td><i class='fa fa-eye w3-blue w3-padding-tiny'></i></td>";
-          echo "<td>".$row['alias']."</td>";
-		  echo "<td>";
-		  foreach($pdo->query( 'select realName from SubPlace, CommentSubPlace where SubPlace.name = CommentSubPlace.name and CommentSubPlace.commentID = '.$luck.';' ) as $brow){;
-		  echo $brow['realName']."</br>";
-		  };
-		  echo "</td>";
-          echo "<td>".$row['grade']." / 5 </td>";
-          echo "<td>".$row['kommentar']."</td>";
-          echo "<td>".$row['date']."</td>";
- 
-        
-        ?>  
-		   <td class="comment-delete">
-		   <form id="cucdel<?php echo $row['commentID']; ?>">
-		   <input type="hidden" name="commentID" value="<?php echo $row['commentID']; ?>">
-           <button type="button" onclick="SendForm('comments', 'comments', 'cucdel<?php echo $row['commentID']; ?>');">radera</button>
-	       </form>
+<div class="w3-row-padding w3-panel w3-card-8 w3-round-xlarge" style=" border-color:lightblue; border-style: solid; border-width: 5px;">
+  <div class="w3-threethird">
+    <h3>Kund kommentarer <b>48h</b></h3>
+    <h5>Antal kommentarer: <?php print_r($d);?></h5>
+    <table class="w3-table w3-striped w3-bordered w3-border w3-hoverable w3-white">
+      <tr>
+        <tr>
+          <th><u>Datum</u></th>
+          <th>Alias</th>
+          <th>Påverkade sträckor</th>
+          <th>Betyg</th>
+          <th>Kommentar</th>
+          <th></th>
+          <?php     
+          foreach($pdo->query( 'SELECT * FROM Commenta order by commentID desc;' ) as $row){
+            $luck = $row ['commentID'];
+            echo "<tr><td>".$row['date']."</td>";
+            echo "<td>".$row['alias']."</td>";
+            echo "<td>";
+            foreach($pdo->query( 'select realName from SubPlace, CommentSubPlace where SubPlace.name = CommentSubPlace.name and CommentSubPlace.commentID = '.$luck.';' ) as $brow){
+              echo $brow['realName']."</br>";
+            }
+            echo "</td>";
+            echo "<td>".$row['grade']." / 5 </td>";
+            echo "<td>".$row['kommentar']."</td>";
+
+
+            ?>  
+            <td>
+             <form id="cucdel<?php echo $row['commentID']; ?>">
+               <input type="hidden" name="commentID" value="<?php echo $row['commentID']; ?>">
+               <button type="button" onclick="SendForm('comments', 'comments', 'cucdel<?php echo $row['commentID']; ?>');">Radera</button>
+             </form>
 
            </td>
-		</tr>
-		   
-		<?php
-        }
-        ?>
-		
-      </table>
-    </div>
-</div>
+         </tr>
+
+         <?php
+       }
+       ?>
+
+     </table>
+   </div>
+ </div>
+ </div>
+ </div>
 
